cfd2b8e6c5aacbf16666c543858064246d6c7440
Madhavi Dobariya <madhavigabani@gmail.com>
2024-06-24 19:46:16 +0530

    feat: initial release

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { BrowserRouter } from "react-router-dom";
+import Pagemain from './Pages/Pagemain/Pagemain';
+
+function App() {
+  return (
+    <>
+     <BrowserRouter>
+      <Pagemain />
+    </BrowserRouter>
+    
+    </>
+  )
+}
+
+export default App
